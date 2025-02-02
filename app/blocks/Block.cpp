acb02d58b0be5339f97df450b320ee3d418a8197
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-02-02 16:16:15 +0300

    Смотри прошлый коммит

--- app/blocks/Block.cpp
+++ app/blocks/Block.cpp
@@ -1,57 +1,26 @@
 #include "Block.h"
-#include "../utils/types/Types.h"
 
-Block::Block() : id(0)
-    {
-        cellSize = 30;
-        rotationState = 0;
-        rowOffset = 0;
-        colOffset = 0;
-        colors = GetCellColors();
-    }
+Block::Block()
+{
+    cellSize = 30;
+    rotationState = 0;
+    rowOffset = 0;
+    colOffset = 0;
+    colors = GetCellColors();
+}
 
 void Block::DrawBlock()
+{
+    std::vector<Position> titles = cells[rotationState];
+    for (Position i : titles)
     {
-        std::vector<Position> tiles = GetCellsPositions();
-        for (Position i: tiles)
-            {
-                DrawRectangle(static_cast<int>(i.col) * static_cast<int>(cellSize) + 1,
-                              static_cast<int>(i.row) * static_cast<int>(cellSize) + 1,
-                              static_cast<int>(cellSize) - 1,
-                              static_cast<int>(cellSize) - 1,
-                              colors[id]);
-            }
+        DrawRectangle(i.col * cellSize + 1, i.row * cellSize + 1, cellSize - 1,
+                      cellSize - 1, colors[id]);
     }
+}
 
-void Block::Move(uint r, uint c)
-    {
-        rowOffset += r;
-        colOffset += c;
-    }
-
-std::vector<Position> Block::GetCellsPositions()
-    {
-        std::vector<Position> tiles = cells[rotationState];
-        std::vector<Position> movedTiles;
-        for (Position i: tiles)
-            {
-                Position newPosition = Position(i.row + rowOffset, i.col + colOffset);
-                movedTiles.push_back(newPosition);
-            }
-        return movedTiles;
-    }
-
-void Block::Rotate()
-    {
-        rotationState++;
-        if (rotationState == (int) cells.size()) rotationState = 0;
-    }
-
-void Block::UndoRotation()
-    {
-        rotationState--;
-        if (rotationState == -1)
-            {
-                rotationState = cells.size() - 1;
-            }
-    }
+void Block::Move(int r, int c)
+{
+    rowOffset += r;
+    colOffset += c;
+}
